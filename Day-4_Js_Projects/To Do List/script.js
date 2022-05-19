36a3dab063033eb35a2f25326f5028c713820b79
Naitik Patel <naitik2002patel@gmail.com>
2022-05-19 12:17:46 +0530

    Day-4 To Do List

---
+++ Day-4_Js_Projects/To Do List/script.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById('btn1');
+let list = document.getElementById('list');
+let input1 = document.getElementById('input1');
+
+btn.addEventListener('click',function(){
+    var para = document.createElement('p');
+    para.innerHTML=input1.value;
+    console.log(para.innerText);
+    list.appendChild(para);
+})
+
+console.log("Hello")
