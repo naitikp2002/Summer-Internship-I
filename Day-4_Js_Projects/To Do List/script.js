a44f2ca183f65dd32028a93e01050dfa8dd5c4b5
Naitik Patel <naitik2002patel@gmail.com>
2022-05-21 21:30:55 +0530

    minor change

--- Day-4_Js_Projects/To Do List/script.js
+++ Day-4_Js_Projects/To Do List/script.js
@@ -5,15 +5,13 @@ let input1 = document.getElementById('input1');
 btn.addEventListener('click',function(){
     var para = document.createElement('p');
     para.innerHTML=input1.value;
-    console.log(para.innerText);
     list.appendChild(para);
+
     input1.value='';
     para.addEventListener('click',function(){
         para.style.textDecoration="line-through";
     })
     para.addEventListener('dblclick',function(){
-        list.removeChild(para)
+        list.removeChild(para);
     })
 })
-
-console.log("Hello")
