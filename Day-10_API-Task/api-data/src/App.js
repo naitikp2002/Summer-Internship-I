37119033d623f0b8a447292417401cac48cd7dc3
Naitik Patel <naitik2002patel@gmail.com>
2022-05-31 19:47:34 +0530

    Fatch data from api

--- Day-10_API-Task/api-data/src/App.js
+++ Day-10_API-Task/api-data/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Table from "./Table";
+
+const baseURL = "https://jsonplaceholder.typicode.com/users";
 
 function App() {
+  const handleclick = () => {};
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setData(response.data);
+    });
+  }, []);
   return (
     <div className="App">
-     
+      <Table data={data}/>
     </div>
   );
 }
---
+++ Day-10_API-Task/api-data/src/Table.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./App.css";
+
+function Table({data}) {
+  return (
+    <div className='Table'><table>
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>username</th>
+      <th>email</th>
+      <th>Address</th>
+      <th>phone</th>
+      <th>website</th>
+      <th>Company</th>
+    </tr>
+  {data.map((items, key) => {
+    return <tr>
+      <td>{items.id}</td>
+      <td>{items.name}</td>
+      <td>{items.username}</td>
+      <td>{items.email}</td>
+      <td>{items.address.street}, {items.address.suite}, {items.address.city}, {items.address.zipcode}</td>
+      <td>{items.phone}</td>
+      <td>{items.website}</td>
+      <td>{items.company.name}, {items.company.chatchPhrase}, {items.company.bs}</td>
+           </tr>;
+  })}
+  </table></div>
+  )
+}
+
+export default Table
