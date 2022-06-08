0ad74159842527a4e9371df376bed81da0a8afa4
Naitik Patel <naitik2002patel@gmail.com>
2022-06-08 22:49:08 +0530

    Week-3 Added

--- Day-12_ValidatorForm_npm/validator-form/src/App.js
+++ Day-12_ValidatorForm_npm/validator-form/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>First Name</label>
+      <input {...register('firstName')} /><br/>
+      <label>Last Name</label>
+      <input {...register('lastName', { required: true })} />
+      {errors.lastName && <p>Last name is required.</p>}<br/>
+      <label>Age</label>
+      <input {...register('age', { pattern: /\d+/ })} />
+      {errors.age && <p>Please enter number for age.</p>}<br/>
+      <input type="submit" />
+    </form>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <label>Name: </label>
+//       <input type={"text"}/>
+//       <br/>
+
+//       <label>Email: </label>
+//       <input type={"text"}/>
+//       <br/>
+//       <label>Phone: </label>
+//       <input type={"text"}/>
+//       <br/>
+//       <label>age: </label>
+//       <input type={"text"}/>
+//       <br/>
+//       <button type="submit">Submit</button>
+//     </div>
+//   );
+// }
 
 export default App;
